b2b7f79437468618fe949ee76da437d9fee2f2f5
Mr. Meo <20571336+trananhmanh89@users.noreply.github.com>
2021-12-09 09:56:21 +0700

    load assets

--- assets/command-palette/vite.config.js
+++ assets/command-palette/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
     return {
         plugins: [vue()],
         // config
--- ffcommand.php
+++ ffcommand.php
@@ -3,6 +3,7 @@
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Registry\Registry;
 
 defined('_JEXEC') or die;
 
@@ -20,7 +21,34 @@ class PlgSystemFFCommand extends CMSPlugin
             $attribs = array('type' => 'module', 'crossorigin' => true);
             HTMLHelper::script('https://localhost:3000/main.js', array(), $attribs);
         } else {
+            $this->loadAssets('main.js');
+        }
+    }
 
+    protected function loadAssets($name)
+    {
+        $dist = 'plugins/system/ffcommand/assets/command-palette/dist/';
+        $content = file_get_contents(JPATH_ROOT . '/' . $dist . 'manifest.json');
+        $manifest = json_decode($content);
+
+        if (isset($manifest->{$name})) {
+            $data = $manifest->{$name};
+
+            if (isset($data->file)) {
+                HTMLHelper::script($dist . $data->file, array(), array('type'=> 'module'));
+            }
+
+            if (isset($data->css)) {
+                foreach ($data->css as $css) {
+                    HTMLHelper::stylesheet($dist . $css);
+                }
+            }
+
+            if (isset($data->imports)) {
+                foreach ($data->imports as $imp) {
+                    $this->loadAssets($imp);
+                }
+            }
         }
     }
 }
